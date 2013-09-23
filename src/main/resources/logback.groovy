8513ceeb4d67a528517208cb76893fe5ae8910eb
toddsarratt <todd.sarratt@gmail.com>
2013-09-23 17:55:21 -0400

    Added log rotation and pruning to logback.groovy

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,5 +1,6 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-import ch.qos.logback.core.FileAppender
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 
 import static ch.qos.logback.classic.Level.DEBUG
 
@@ -7,8 +8,12 @@ scan()
 
 def PROJECT_HOME = "/home/tsarratt/codeDir/GaussTrader"
 
-appender("FILE", FileAppender) {
+appender("FILE", RollingFileAppender) {
   file = "${PROJECT_HOME}/logs/GaussTrader.log"
+  rollingPolicy(TimeBasedRollingPolicy) {
+    fileNamePattern = "${PROJECT_HOME}/logs/GaussTrader.%d.log"
+    maxHistory = 30
+  }
   encoder(PatternLayoutEncoder) {
     pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS zzz} [%thread] %-5level %logger{0} - %msg%n"
   }
