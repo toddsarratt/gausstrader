48eed8226d31408360785864e2a00fa772172341
toddsarratt <todd.sarratt@gmail.com>
2013-10-21 18:11:39 -0400

    Updated unit tests to reflect cost basis adjustment when adding new position

--- src/test/java/net/toddsarratt/GaussTrader/PortfolioExerciseShortPut.java
+++ src/test/java/net/toddsarratt/GaussTrader/PortfolioExerciseShortPut.java
@@ -22,22 +22,22 @@ public class PortfolioExerciseShortPut {
 	shortPutPosition.setLongPosition(false);
 	shortPutPosition.setNumberTransacted(1);
 	shortPutPosition.setPriceAtOpen(0.75);
-	shortPutPosition.setCostBasis(-75.00);
-	shortPutPosition.setClaimAgainstCash(shortPutPosition.calculateClaimAgainstCash());
+	shortPutPosition.calculateCostBasis();
+	shortPutPosition.calculateClaimAgainstCash();
 
 	portfolio.addNewPosition(shortPutPosition);
-	assertEquals(portfolio.calculateTotalCash(), 1_000_000.00);
+	assertEquals(portfolio.calculateTotalCash(), 1_000_075.00);
 	assertEquals(portfolio.getReservedCash(), 10_000.00);
-	assertEquals(portfolio.getFreeCash(), 990_000.00);
+	assertEquals(portfolio.getFreeCash(), 990_075.00);
         assertEquals(portfolio.countUncoveredLongStockPositions(testStock), 0);
         assertEquals(portfolio.numberOfOpenStockLongs(testStock), 0);
 	assertEquals(portfolio.numberOfOpenPutShorts(testStock), 1);
 
 	portfolio.exerciseOption(shortPutPosition);
 
-	assertEquals(portfolio.calculateTotalCash(), 990_000.00);
+	assertEquals(portfolio.calculateTotalCash(), 990_075.00);
         assertEquals(portfolio.getReservedCash(), 0.00);
-        assertEquals(portfolio.getFreeCash(), 990_000.00);
+        assertEquals(portfolio.getFreeCash(), 990_075.00);
 	assertEquals(portfolio.countUncoveredLongStockPositions(testStock), 1);
 	assertEquals(portfolio.numberOfOpenStockLongs(testStock), 1);
 	assertEquals(portfolio.numberOfOpenPutShorts(testStock), 0);
