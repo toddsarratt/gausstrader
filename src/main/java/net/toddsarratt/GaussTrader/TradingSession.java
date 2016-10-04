41eddeb25bbb11dd3bae4913b739af35119f1f95
toddsarratt <todd.sarratt@gmail.com>
2016-10-04 16:56:23 -0400

    Created TradingStrategy class. Refactored methods out of TradingSession into TradingStrategy.

--- src/main/java/net/toddsarratt/GaussTrader/TradingSession.java
+++ src/main/java/net/toddsarratt/GaussTrader/TradingSession.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.sql.SQLException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -63,7 +62,7 @@ class TradingSession {
 			InstantPrice currentInstantPrice = market.lastTick(stock.getTicker());
 			BigDecimal stockPrice = currentInstantPrice.getPrice();
 			dataStore.writeStockPrice(stock.getTicker(), currentInstantPrice);
-			PriceBasedAction actionToTake = findActionToTake(stock, stockPrice);
+			PriceBasedAction actionToTake = TradingStrategy.findActionToTake(stock, stockPrice, portfolio);
 			if (actionToTake.isActionable()) {
 				takeActionOnStock(stock, actionToTake);
 			}
@@ -173,84 +172,8 @@ class TradingSession {
 		}
 	}
 
-	private PriceBasedAction findActionToTake(Stock stock, BigDecimal stockPrice) {
-	    /* Decide if a security's current price triggers a predetermined event */
-		LOGGER.debug("Entering findActionToTake(Stock {})", stock.getTicker());
-		LOGGER.debug("Comparing current price ${} against Bollinger Bands {}", stockPrice, stock.describeBollingerBands());
-		if (stockPrice.compareTo(stock.getBollingerBand(1)) >= 0) {
-			return createCallAction(stock, stockPrice);
-		}
-		if (stock.getFiftyDma().compareTo(stock.getTwoHundredDma()) < 0) {
-			LOGGER.info("Stock {} 50DMA < 200DMA. No further checks.", stock.getTicker());
-			return PriceBasedAction.DO_NOTHING;
-		}
-		if (stockPrice.compareTo(stock.getBollingerBand(3)) <= 0) {
-			return createPutAction(stock, stockPrice);
-		}
-		LOGGER.info("Stock {} at ${} is within Bollinger Bands", stock.getTicker(), stockPrice);
-		return PriceBasedAction.DO_NOTHING;
-	}
 
-	private PriceBasedAction createCallAction(Stock stock, BigDecimal stockPrice) {
-		LOGGER.debug("Entering createCallAction(Stock {}, BigDecimal {})", stock.getTicker(), stockPrice);
-		if (portfolio.countUncoveredLongStockPositions(stock) < 1) {
-			LOGGER.info("Open long {} positions is equal or less than current short calls positions. Taking no action.", stock.getTicker());
-			return PriceBasedAction.DO_NOTHING;
-		}
-		if (stockPrice.compareTo(stock.getBollingerBand(2)) >= 0) {
-			LOGGER.info("Stock {} at ${} is above 2nd Bollinger Band of {}", stock.getTicker(), stockPrice, stock.getBollingerBand(2));
-			return new PriceBasedAction(stockPrice,
-					true,
-					"SELL",
-					SecurityType.CALL,
-					Math.min(portfolio.numberOfOpenStockLongs(stock), 5));
-		}
-	  /* TODO : Consider removing this if statement. We should not be in this method if the condition wasn't met, though
-	  * it does protect against misuse of the API. Also, why is the PriceBasedAction exactly the same as above? */
-		if (stockPrice.compareTo(stock.getBollingerBand(1)) >= 0) {
-			LOGGER.info("Stock {} at ${} is above 1st Bollinger Band of {}", stock.getTicker(), stockPrice, stock.getBollingerBand(1));
-			return new PriceBasedAction(stockPrice,
-					true,
-					"SELL",
-					SecurityType.CALL,
-					Math.min(portfolio.numberOfOpenStockLongs(stock), 5));
-		}
-		return PriceBasedAction.DO_NOTHING;
-	}
 
-	private PriceBasedAction createPutAction(Stock stock, BigDecimal stockPrice) {
-		LOGGER.debug("Entering createPutAction(Stock {}, BigDecimal {})", stock.getTicker(), stockPrice);
-		int openPutShorts = portfolio.numberOfOpenPutShorts(stock);
-		int maximumContracts = Constants.STOCK_PCT_OF_PORTFOLIO.divide(Constants.BIGDECIMAL_ONE_HUNDRED, 3, RoundingMode.HALF_UP)
-				.divide(stockPrice.multiply(Constants.BIGDECIMAL_ONE_HUNDRED), 3, RoundingMode.HALF_UP)
-				.multiply(portfolio.calculateNetAssetValue())
-				.intValue();
-		if (stockPrice.compareTo(stock.getBollingerBand(5)) <= 0) {
-			LOGGER.info("Stock {} at ${} is below 3rd Bollinger Band of {}", stock.getTicker(), stockPrice, stock.getBollingerBand(5));
-			if (openPutShorts < maximumContracts) {
-				return new PriceBasedAction(stockPrice, true, "SELL", SecurityType.PUT, Math.max(maximumContracts / 4, 1));
-			}
-			LOGGER.info("Open short put {} positions equals {}. Taking no action.", stock.getTicker(), openPutShorts);
-			return PriceBasedAction.DO_NOTHING;
-		}
-		if (stockPrice.compareTo(stock.getBollingerBand(4)) <= 0) {
-			LOGGER.info("Stock {} at ${} is below 2nd Bollinger Band of {}", stock.getTicker(), stockPrice, stock.getBollingerBand(4));
-			if (openPutShorts < maximumContracts / 2) {
-				return new PriceBasedAction(stockPrice, true, "SELL", SecurityType.PUT, Math.max(maximumContracts / 4, 1));
-			}
-			LOGGER.info("Open short put {} positions equals {}. Taking no action.", stock.getTicker(), openPutShorts);
-			return PriceBasedAction.DO_NOTHING;
-		}
-	  /* TODO : Consider removing this if statement. We should not be in this method if the condition wasn't met */
-		if (stockPrice.compareTo(stock.getBollingerBand(3)) <= 0) {
-			LOGGER.info("Stock {} at ${} is below 1st Bollinger Band of {}", stock.getTicker(), stockPrice, stock.getBollingerBand(3));
-			if (openPutShorts < maximumContracts / 4) {
-				return new PriceBasedAction(stockPrice, true, "SELL", SecurityType.PUT, Math.max(maximumContracts / 4, 1));
-			}
-			LOGGER.info("Open short put {} positions equals {}. Taking no action.", stock.getTicker(), openPutShorts);
-		}
-		return PriceBasedAction.DO_NOTHING;
-	}
 
 	/**
 	 * If within two days with expiry exercise ITM options. If underlyingTicker < optionStrike then stock is PUT to Portfolio
